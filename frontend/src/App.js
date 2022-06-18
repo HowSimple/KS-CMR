88235edb7c616a9689e6200827f490708150fa73
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2022-06-18 11:14:30 -0500

    Simple react form

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './Form.js';
+function App() {
+  return (
+    <div className="App">
+
+      <header className="App-header">
+          <Form />
+          <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
